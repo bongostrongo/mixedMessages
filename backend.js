bd8cc095e232fe995a20a0aea0d801cb11643048
bongostrongo <56402832+bongostrongo@users.noreply.github.com>
2021-05-08 08:54:57 +0200

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,29 @@
+let verbs = require('./verbs.js');
+let adjectives = require('./adjectives.js');
+let things = require('./things.js');
+
+let vowels = ['a','e','i','o','u']
+
+const randomize = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+const randVerb = () => {
+    return randomize(verbs);
+}
+const randAdjective = () => {
+    return randomize(adjectives);
+}
+
+const randThing = () => {
+    
+    let thing = randomize(things);
+    let article = vowels.includes(thing[0]) ? 'an' : 'a'
+    return article + ' ' + thing + '!';
+}
+
+const spitMessage = () => {
+    document.getElementById("message").innerHTML='Try ' + randAdjective() + ' ' + randVerb() + ' ' + randThing();
+
+};
+
+console.log('Try ' + randAdjective() + ' ' + randVerb() + ' ' + randThing());
